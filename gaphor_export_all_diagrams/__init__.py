becfd06e996d58b671da2497f62f8383b283e390
Frank Vanbever <frank.vanbever@mind.be>
2024-04-25 17:25:08 +0200

    Add actual PNG export code
    The pngs are exported to a directory you can select.
    
    Signed-off-by: Frank Vanbever <frank.vanbever@mind.be>

--- gaphor_export_all_diagrams/__init__.py
+++ gaphor_export_all_diagrams/__init__.py
@@ -1,23 +1,52 @@
 import logging
-import gi
+from pathlib import Path
 
 from gaphor.abc import ActionProvider, Service
 from gaphor.action import action
 from gaphor.i18n import gettext
+from gaphor.diagram.export import save_png, escape_filename
+from gaphor.core.modeling import Diagram
+from gaphor.plugins.diagramexport import exportcli
 
-from gi.repository import Adw
+from gi.repository import Gtk
 
 log = logging.getLogger(__name__)
 
 
 class ExportAllPlugin(Service, ActionProvider):
 
-    def __init__(self, main_window, tools_menu):
+    def __init__(self, main_window=None, export_menu=None, element_factory=None):
         self.main_window = main_window
-        tools_menu.add_actions(self)
+        self.export_menu = export_menu
+        self.factory = element_factory
+        if export_menu:
+            export_menu.add_actions(self)
 
     def shutdown(self):
         pass
+
+    def export_pngs_handler(self, path):
+        log.info(f"Outputting diagrams to {path}")
+        for diagram in self.factory.select(Diagram):
+            odir = f"{path}/{exportcli.pkg2dir(diagram.owner)}"
+
+            # just diagram name
+            dname = escape_filename(diagram.name)
+            # full diagram name including package path
+            pname = f"{odir}/{dname}"
+
+            odir = odir.replace(" ", "_")
+            dname = dname.replace(" ", "_")
+
+            outfilename = f"{odir}/{dname}.png"
+
+            if not Path(odir).exists():
+                log.debug("creating dir %s", odir)
+                Path(odir).mkdir(parents=True)
+
+            log.debug("rendering: %s -> %s...", pname, outfilename)
+
+            save_png(outfilename, diagram)
 
     @action(
         name="export-all-diagrams",
@@ -25,12 +54,14 @@ class ExportAllPlugin(Service, ActionProvider):
         tooltip=gettext("Export all diagrams as PNG diagrams in a specified directory"),
     )
     def export_all_action(self):
-        window = self.main_window.window
+        dialog = Gtk.FileDialog.new()
+        dialog.set_title(gettext("Export all diagrams"))
 
-        dialog = Adw.MessageDialog.new(window, gettext("Export all diagrams"))
-        dialog.set_body(gettext("This is a test"))
-        dialog.add_response("close", gettext("Close"))
-        dialog.set_close_response("close")
+        def response(dialog, result):
+            if result.had_error():
+                return
 
-        dialog.set_visible(True)
+            folder = dialog.select_folder_finish(result).get_path()
+            self.export_pngs_handler(folder)
 
+        dialog.select_folder(callback=response)
